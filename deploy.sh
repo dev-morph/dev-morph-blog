6d9288249e2f12992d3beaed683fbd75c928b259
dev-morph <112484043+dev-morph@users.noreply.github.com>
2024-01-23 01:11:51 +0900

    이미지 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
-npm run build
 pm2 list
 echo " === got pm2 list === "
 pm2 stop blog
+npm run build
 pm2 start npm --name "blog" -- start
 pm2 save
