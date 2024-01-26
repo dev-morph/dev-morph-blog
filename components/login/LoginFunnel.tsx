406b4ebcdaced973f38e6adc831b02d6402106c2
dev-morph <112484043+dev-morph@users.noreply.github.com>
2024-01-26 20:13:10 +0900

    button 컴포넌트 추가

--- components/login/LoginFunnel.tsx
+++ components/login/LoginFunnel.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { useState, useRef, FormEvent } from 'react';
+import Top02 from '@/morph-lib/components/Top/Top02';
+import Spacing from '@/morph-lib/components/Spacing';
 import { z } from 'zod';
 import useForm from '@/morph-lib/hooks/useForm';
+import Button from '@/morph-lib/components/Button';
 
 export default function LoginFunnel() {
 	const formRef = useRef<HTMLFormElement>(null);
 	const schema = z.object({
-		id: z.string().min(1, { message: '필수 입력 항목입니다.' }),
+		username: z.string().min(1, { message: '필수 입력 항목입니다.' }),
 		password: z.string().min(1, { message: '필수 입력 항목입니다.' }),
 	});
 	const { valid, formData } = useForm(formRef, schema);
@@ -20,16 +23,24 @@ export default function LoginFunnel() {
 
 	return (
 		<>
+			<Top02 textAlign="center">Login</Top02>
+			<Spacing size={30} />
 			<form ref={formRef} onSubmit={loginSubmitHandler}>
 				<div>
-					<label htmlFor="id">ID</label>
-					<input id="id" name="id" type="text" />
+					<label htmlFor="username">username</label>
+					<input id="username" name="username" type="text" />
 				</div>
 				<div>
-					<label htmlFor="password">PASSWORD</label>
+					<label htmlFor="password">password</label>
 					<input id="password" name="password" type="text" />
 				</div>
-				<button>로그인</button>
+				<Spacing size={15} />
+				<Button
+					onClick={() => console.log('hmmm')}
+					fontSize="var(--size-4)"
+				>
+					로그인
+				</Button>
 			</form>
 		</>
 	);
---
+++ morph-lib/components/Button.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren } from 'react';
+import classes from '@morphlib/sass/Button.module.scss';
+
+type ButtonProps = PropsWithChildren<{
+	type?: 'primary' | 'dark';
+	style?: 'fill';
+	display?: 'inline' | 'block' | 'full';
+	btnColor?: string;
+
+	onClick?: React.MouseEventHandler<HTMLElement>;
+	disabled?: boolean;
+	fontSize?: string;
+}>;
+
+export default function Button(props: ButtonProps) {
+	const {
+		type = 'primary',
+		style = 'fill',
+		display = 'inline',
+		btnColor,
+		disabled,
+		children,
+		fontSize,
+		...rest
+	} = props;
+
+	return (
+		<button
+			{...rest}
+			className={`${classes.button} ${classes[type]}`}
+			style={{ backgroundColor: btnColor }}
+			disabled={disabled}
+		>
+			<span className={classes.btn__text} style={{ fontSize: fontSize }}>
+				{children}
+			</span>
+		</button>
+	);
+}
