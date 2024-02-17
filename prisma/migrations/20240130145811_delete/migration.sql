ca6875f21ab0a18c4a7bbd84a4692772431f292e
dev-morph <112484043+dev-morph@users.noreply.github.com>
2024-02-18 08:13:33 +0900

    prisma migration sql update

--- prisma/migrations/20240130145811_delete/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `category` on the `Post` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Post` DROP COLUMN `category`;
--- prisma/migrations/20240217231217_init_ddl/migration.sql
+++ prisma/migrations/20240217231217_init_ddl/migration.sql
@@ -34,7 +34,7 @@ CREATE TABLE `Like` (
 CREATE TABLE `Image` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `url` VARCHAR(255) NOT NULL,
-    `alt` VARCHAR(255) NULL,
+    `filename` VARCHAR(255) NOT NULL,
     `post_id` INTEGER NOT NULL,
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
@@ -46,7 +46,6 @@ CREATE TABLE `Image` (
 CREATE TABLE `Post` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
-    `category` INTEGER NOT NULL,
     `contents` TEXT NOT NULL,
     `hits` INTEGER NOT NULL DEFAULT 0,
     `thumbnail` VARCHAR(255) NULL,
@@ -72,6 +71,7 @@ CREATE TABLE `Category` (
     `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updated_at` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
 
+    UNIQUE INDEX `Category_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
