30917e57ce0389536631dfd518c42ff0c5782592
dev-morph <112484043+dev-morph@users.noreply.github.com>
2024-04-30 18:54:22 +0900

    수정

--- morph-lib/hooks/useModal.tsx
+++ morph-lib/hooks/useModal.tsx
@@ -1,28 +1,41 @@
-import { useRef, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import classes from '@morphlib/sass/Modal.module.scss';
 import Border from '../components/Border';
 import Button from '../components/Button';
+import ModalBackground from '../components/Modal/ModalBackground';
 import CloseIcon from '@/components/ui/icons/close-icon';
 
 type ModalPropsType = {
 	title?: React.ReactNode;
 	body?: React.ReactNode;
 	buttons?: React.ReactNode;
+	children?: React.ReactNode;
 };
 
 type UseModalProps = {
 	confirmCallback?: Function;
 	cancleCallback?: Function;
+	buttonHide?: Boolean;
+	titleHide?: Boolean;
 };
 
 export default function useModal({
 	cancleCallback,
 	confirmCallback,
+	buttonHide = false,
+	titleHide = false,
 }: UseModalProps) {
 	const [isOpen, setIsOpen] = useState(false);
 	const modalRef = useRef<HTMLDivElement>(null);
 
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'unset';
+		}
+	}, [isOpen]);
 	function openDialog() {
 		setIsOpen(true);
 	}
@@ -62,9 +75,14 @@ export default function useModal({
 			cancleCallback();
 		}
 	}
-	CloseIcon;
-	// function Modal({ children }: { children?: React.ReactNode }) {
-	function Modal({ title, body, buttons }: ModalPropsType) {
+
+	const Modal = memo(function Modal({
+		title,
+		body,
+		buttons,
+		children,
+	}: ModalPropsType) {
+		console.log('check modal rerender');
 		const buttonComponent = buttons ? (
 			buttons
 		) : (
@@ -87,45 +105,61 @@ export default function useModal({
 		);
 
 		if (isOpen) {
-			return createPortal(
-				<div
-					className={classes.modal__backdrop}
-					onClick={modalClickHandler}
-				>
-					<div className={classes.modal} ref={modalRef}>
-						<div className={classes.modal__header}>
-							{title}
-							<div className={classes.close__icon}>
-								<CloseIcon size="20" />
-							</div>
-						</div>
-						<Border withOutSpacing={true} borderWidth="0.75px" />
-						<div className={classes.modal__body}>{body}</div>
-						<Border withOutSpacing={true} borderWidth="0.75px" />
-						<div className={classes.modal__btns}>
-							{buttonComponent}
-							{/* {buttons}
-							<Button width="100px" onClick={successHandler}>
-								확인
-							</Button>
-							<Button
-								width="100px"
-								onClick={failHandler}
-								btnColor="var(--button-bgColor-muted)"
-								color="black"
-								btnStyle={{
-									border: '1px solid var(--border-default-color)',
-								}}
+			return (
+				<>
+					{createPortal(
+						<ModalBackground
+							modalRef={modalRef}
+							setIsOpen={setIsOpen}
+						/>,
+						document.querySelector('#backdrop-root') as HTMLElement
+					)}
+					{createPortal(
+						<div className={classes.modal} ref={modalRef}>
+							<div
+								className={`${classes.modal__header} ${
+									titleHide && classes.hidden
+								}`}
 							>
-								취소
-							</Button> */}
-						</div>
-					</div>
-				</div>,
-				document.querySelector('#dialogs') as HTMLElement
+								{title}
+								<div
+									className={classes.close__icon}
+									onClick={closeDialog}
+								>
+									<CloseIcon size="20" />
+								</div>
+							</div>
+							{!titleHide ? (
+								<Border
+									withOutSpacing={true}
+									borderWidth="0.75px"
+								/>
+							) : (
+								<></>
+							)}
+							<div className={classes.modal__body}>{body}</div>
+							{!titleHide ? (
+								<Border
+									withOutSpacing={true}
+									borderWidth="0.75px"
+								/>
+							) : (
+								<></>
+							)}
+							<div
+								className={`${classes.modal__btns} ${
+									buttonHide && classes.hidden
+								}`}
+							>
+								{buttonComponent}
+							</div>
+						</div>,
+						document.querySelector('#overlay-root') as HTMLElement
+					)}
+				</>
 			);
 		}
-	}
+	});
 
 	return { Modal, openDialog, closeDialog };
 }
